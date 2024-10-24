b0919ae126276f1eb1d4448a9313f7bbceeaf3d2
nanan <wangnannan@bonc.com.cn>
2024-10-24 15:09:07 +0800

    添加日志文件，日志增加traceId。

---
+++ src/main/java/com/github/nan/web/core/filter/TraceIdFilter.java
@@ -0,0 +1,28 @@
+package com.github.nan.web.core.filter;
+
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.UUID;
+
+
+/**
+ * @author NanNan Wang
+ */
+
+@Component
+public class TraceIdFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        String traceId = UUID.randomUUID().toString();
+        MDC.put("traceId", traceId);
+        try {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+            MDC.clear(); // 清除 MDC 中的 traceId，避免影响后续请求
+        }
+    }
+}
--- src/main/java/com/github/nan/web/demos/web/DemoController.java
+++ src/main/java/com/github/nan/web/demos/web/DemoController.java
@@ -2,6 +2,7 @@ package com.github.nan.web.demos.web;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "请求规范")
 @RestController
 @RequestMapping("/demos")
+@Slf4j
 public class DemoController {
 
 
     @ApiOperation(value = "向世界问好")
     @GetMapping("/hello")
     public String hello() {
+        log.info("first api , use mdc to create trace Id");
         return "hello world!";
     }
 
